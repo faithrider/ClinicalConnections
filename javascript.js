63f57c64349022ae17bf534540601b028669dfd9
Faith <fmhrider@gmail.com>
2023-02-25 17:34:56 -0500

    change jc

--- javascript.js
+++ javascript.js
@@ -31,3 +31,20 @@ class organization_profile {
     "John Doe III",
     "www.websitethree.com"
   );
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    const navLinks = document.querySelectorAll("nav a");
+    const sections = document.querySelectorAll("section");
+  
+    navLinks.forEach(function(link) {
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const target = this.getAttribute("href");
+        const targetSection = document.querySelector(target);
+        sections.forEach(function(section) {
+          section.classList.remove("active");
+        });
+        targetSection.classList.add("active");
+      });
+    });
+  });
