80532fb13b81b8ee693bcec728ffbb4b64eb5cac
Faith <fmhrider@gmail.com>
2023-02-25 15:17:54 -0500

    created js file

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// Declaration
+class organization_info {
+    constructor(organization_name, trial_list, ) {
+      this.organization_name = organization_name;
+      this.trial_list = trial_list;
+    }
+
+    
+
+
+
+  };
+  
