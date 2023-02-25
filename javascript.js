d9624148bddb22778409b9b9b9d3ada073d7ddcf
Faith <fmhrider@gmail.com>
2023-02-25 16:02:10 -0500

    set up mock company js profiles

--- javascript.js
+++ javascript.js
@@ -1,13 +1,33 @@
 // Declaration
-class organization_info {
-    constructor(organization_name, trial_list, ) {
-      this.organization_name = organization_name;
+class organization_profile {
+    constructor(name, trial_list, contact_rep, company_website) {
+      this.name = name;
       this.trial_list = trial_list;
+      this.contact_rep = contact_rep;
+      this.company_website = company_website;
     }
-
-    
-
 
 
   };
   
+// Create company profile instances
+  const companyA = new organization_profile(
+    "Clinical Trail Org I",
+    ["CT I - a"],
+    "John Doe I",
+    "www.websiteone.com"
+  );
+
+  const companyB = new organization_profile(
+    "Clinical Trail Org II",
+    ["CT II - a", "CT II - b", "CT II - c"],
+    "John Doe II",
+    "www.websitetwo.com"
+  );
+
+  const companyC = new organization_profile(
+    "Clinical Trail Org III",
+    ["CT III - a", "CT III - b"],
+    "John Doe III",
+    "www.websitethree.com"
+  );
