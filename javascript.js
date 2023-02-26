d3f98a347bed562b13732541f85c5c1f000b52b3
Faith <fmhrider@gmail.com>
2023-02-25 23:35:12 -0500

    card flexbox

--- javascript.js
+++ javascript.js
@@ -1,37 +1,4 @@
-// Declaration
-class organization_profile {
-    constructor(name, trial_list, contact_rep, company_website) {
-      this.name = name;
-      this.trial_list = trial_list;
-      this.contact_rep = contact_rep;
-      this.company_website = company_website;
-    }
-
-
-  };
-  
-// Create company profile instances
-  const companyA = new organization_profile(
-    "Clinical Trail Org I",
-    ["CT I - a"],
-    "John Doe I",
-    "www.websiteone.com"
-  );
-
-  const companyB = new organization_profile(
-    "Clinical Trail Org II",
-    ["CT II - a", "CT II - b", "CT II - c"],
-    "John Doe II",
-    "www.websitetwo.com"
-  );
-
-  const companyC = new organization_profile(
-    "Clinical Trail Org III",
-    ["CT III - a", "CT III - b"],
-    "John Doe III",
-    "www.websitethree.com"
-  );
-
+// makes the correct section selected be the active one shown
   document.addEventListener("DOMContentLoaded", function(event) {
     const navLinks = document.querySelectorAll("nav a");
     const sections = document.querySelectorAll("section");
